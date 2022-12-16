2c761fdb88cd6146fad520044b276b2b09954df0
RegSlinky <thesmile0701@gmail.com>
2022-12-16 18:58:45 +0900

    first_commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,3 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.order(id: "DESC")
-  end
- 
-  def create
-    post = Post.create(content: params[:content])
-    render json:{ post: post }
-  end
+
  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'  
-  post 'posts', to: 'posts#create'
+
 end
